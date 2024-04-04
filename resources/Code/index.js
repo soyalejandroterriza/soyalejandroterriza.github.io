daec0f0d256e4bf46dc23ae8563da56256a59ebf
Alejandro Terriza <soyalejandroterriza@gmail.com>
2024-04-04 23:05:32 +0200

    Más proyectos

--- resources/Code/index.js
+++ resources/Code/index.js
@@ -12,8 +12,18 @@ document.getElementById("linkedin_button").addEventListener("click", function() 
     window.open(url, "_blank");
 });
 
-// Comportamiento del botón Email-me
+// Comportamiento del botón Email
 document.getElementById("linkedin_button").addEventListener("click", function() {
     var url = "mailto:soyalejandroterriza@gmail.com";
     window.open(url);
 });
+
+// Comportamiento del botón Itch.io
+document.getElementById("itch.io_button").addEventListener("click", function() {
+    var url = "https://soyalejandroterriza.itch.io/";
+    window.open(url);
+});
+
+
+
+
