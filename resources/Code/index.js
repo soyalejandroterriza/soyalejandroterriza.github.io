a579ebc0ab1fc818aeff181a6a51b99fff4b3c76
Alejandro Terriza <soyalejandroterriza@gmail.com>
2024-04-04 22:00:27 +0200

    V0.1

--- resources/Code/index.js
+++ resources/Code/index.js
@@ -1,2 +1,19 @@
 
 
+// Comportamiento del botón GitHub
+document.getElementById("github_button").addEventListener("click", function() {
+    var url = "https://github.com/soyalejandroterriza";
+    window.open(url, "_blank");
+});
+
+// Comportamiento del botón LinkedIn
+document.getElementById("linkedin_button").addEventListener("click", function() {
+    var url = "https://www.linkedin.com/in/soyalejandroterriza";
+    window.open(url, "_blank");
+});
+
+// Comportamiento del botón Email-me
+document.getElementById("linkedin_button").addEventListener("click", function() {
+    var url = "mailto:soyalejandroterriza@gmail.com";
+    window.open(url);
+});
